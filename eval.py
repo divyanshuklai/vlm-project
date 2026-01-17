41693d8b6830c28d600fabe4fc1242623f2da0d3
div <divyanshmohanshukla@hotmail.com>
2026-01-17 11:36:13 +0530

    first commit

---
+++ eval.py
@@ -0,0 +1,70 @@
+import os
+import pandas as pd
+from VLMEvalKit.vlmeval.dataset import build_dataset
+from VLMEvalKit.vlmeval.config import supported_VLM
+from VLMEvalKit.vlmeval.inference import infer_data_job
+from VLMEvalKit.vlmeval.smp import get_pred_file_path
+
+from VLMEvalKit.vlmeval.vlm import BaseModel
+
+DATASETS = [
+    "RefCOCO",
+    "GQA_TestDev_Balanced",
+    "DocVQA_TEST",
+    "ChartQA_TEST",
+    "CountBenchQA",
+    "POPE",
+]
+
+def evaluate_vlm(model, workdir, nproc=1):
+    """
+    evaluate your vlm on refCOCO, refCOCO+, refCOCOg, GQA, DocVQA, ChartQA, CountBenchQA, and POPE.
+    
+    :param model: your model, specified using VLMEvalKit.vlmeval.vlm.BaseModel class
+    :param workdir: directory to store the raw results in xlsx, and scores in json under workdir/model_name/.
+
+    note: the specific evals chosen are the same as those reported by moondream.ai for moondream3,
+    which i am using as a reference to build VLMs.
+    """
+
+    model_name = "custom_model"
+    if isinstance(model, str):
+        model_name = model
+        if model_name in supported_VLM:
+            model : BaseModel = supported_VLM[model_name]()
+        else:
+            raise ValueError("The given model name is not supported by VLMEvalKit")
+        
+    elif hasattr(model, "model_name"):
+        model_name = model.model_name
+    
+    results = {}
+
+    for dataset_name in DATASETS:
+        print(f"Evaluating {model_name} on {dataset_name}")
+
+        dataset = build_dataset(dataset_name)
+        if dataset is None:
+            print(f"dataset {dataset_name} not found")
+            continue
+        
+        infer_data_job(
+            model=model,
+            work_dir=workdir,
+            model_name=model_name,
+            dataset=dataset,
+            verbose=True,
+            ignore_failed=True,
+            api_nproc=1,
+        )
+
+        pred_file = get_pred_file_path(
+            work_dir=workdir,
+            model_name=model_name,
+            dataset_name=dataset_name,
+        )
+
+        res = dataset.evaluate(pred_file)
+        results[dataset_name] = res
+
+    return results
